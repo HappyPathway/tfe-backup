413856107f76c8f958739ee5423455329f412def
Dave Arnold <darnold@hashicorp.com>
2018-10-05 12:19:18 -0700

    print org and workspace

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tfe_backup",
-    version="0.2.2",
+    version="1.2.2",
     author="Dave Arnold",
     author_email="dave@happypathway.com",
     description="Utilties for backing up TFE Workspaces as Terraform Code",
